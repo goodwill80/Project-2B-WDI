ac33f918356f5bcdedc466a49a254778b0dd0fc0
kit jonathan ho <j.hokit80@gmail.com>
2016-08-04 01:33:32 +0800

    reviews model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many   :reviews
 
   has_attached_file :product_img, styles: { medium: "350x350>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\Z/
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803162929) do
+ActiveRecord::Schema.define(version: 20160803163153) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160803162929) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "product_id"
   end
 
   create_table "users", force: :cascade do |t|
