244d721b43bf5cc399dcbb79af4254696bc78efe
kit jonathan ho <j.hokit80@gmail.com>
2016-08-03 20:49:33 +0800

    user id migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803115520) do
+ActiveRecord::Schema.define(version: 20160803124819) do
 
   create_table "products", force: :cascade do |t|
     t.string   "country"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160803115520) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "name"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
