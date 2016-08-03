7d23b2f0d65b25001c868e744e788308291c42c8
kit jonathan ho <j.hokit80@gmail.com>
2016-08-03 16:16:08 +0800

    updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,26 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all.order("created_at DESC")
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+        redirect_to products_path
+      else
+        render 'new'
+      end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:country, :place, :description, :subject, :name)
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803062750) do
+ActiveRecord::Schema.define(version: 20160803065049) do
 
   create_table "products", force: :cascade do |t|
     t.string   "country"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160803062750) do
     t.string   "subject"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
 end
