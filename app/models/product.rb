2456d8090aa25c96e82ef9f63b9219b8ad01c10a
kit jonathan ho <j.hokit80@gmail.com>
2016-08-04 17:18:04 +0800

    add images

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,8 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many   :reviews
 
-  has_attached_file :product_img, styles: { medium: "350x350>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\Z/
-  validates_attachment :product_img,
-  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  has_attached_file :product_img, styles: { product_index: "375x300>", book_show: "380x475>" }, default_url: "/images/:style/missing.png"
+ validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\Z/
+  # validates_attachment :product_img,
+  # content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
